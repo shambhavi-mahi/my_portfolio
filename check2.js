527972d8b9112952c2c7447e4d8985ee88d5f839
shambhavimahi23-debug <shambhavimahi23@gmail.com>
2026-08-22 10:52:30 +0530

    chore: remove temp debug scripts

--- check2.js
+++
@@ -1,1 +0,0 @@
-const i=require('lucide-react');const keys=Object.keys(i);console.log(keys.filter(function(k){return k.toLowerCase().indexOf('git')!==-1}).join(', '));  
--- check_icons.js
+++
@@ -1,1 +0,0 @@
-const i=require('lucide-react');console.log(Object.keys(i).slice(0,20).join(', '));  
